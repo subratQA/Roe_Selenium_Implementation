e2ca4745a534e1294f4b6545ddd047085c0ca86f
Subrat Sarkar <subrat.mya@outlook.com>
2020-06-12 00:51:04 +0530

    Updated from My PC

---
+++ Automation.Core/Logging/TraceLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Automation.Core.Logging
+{
+    public class TraceLogger : ILogger
+    {
+        public void Debug(string message)
+        { Trace.TraceInformation(message); }
+
+        public void Debug(string Format, params object[] args)
+        {    Trace.TraceInformation(string.Format(Format, args));  }
+
+        public void Debug(Exception exception, string message)
+        {   Trace.TraceError(message + " " + $"{exception}");   }
+    }
+}
